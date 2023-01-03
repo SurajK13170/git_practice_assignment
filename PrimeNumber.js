c794f002f8f898d1d1fb5ad8a5239a479734299a
Vivek Sharma <vs246246@gmail.com>
2023-01-03 21:07:11 +0530

    Palindrome and Prime Number

--- PrimeNumber.js
+++ PrimeNumber.js
@@ -1,7 +1,10 @@
 let num=6;
 for(let i=2; i<num; i++){
     if(num%i==0){
-    console.log("not prime");
-    break;
+        console.log("Not Prime");
+        break;
+    }
+    else if(i == num-1){
+        console.log("Prime");
     }
 }
