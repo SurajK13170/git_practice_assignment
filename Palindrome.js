fc06e022c715066939654eefcec15b8a6b33c409
Suraj Kumar <bgrills57@gmail.com>
2023-01-03 20:57:01 +0530

    Palindrom and prime number

---
+++ Palindrome.js
@@ -0,0 +1,11 @@
+let name="naman";
+let str="";
+for(let i=name.length; i>=0; i--){
+    str+=name[i]; 
+}
+if(name==str){
+    console.log("true");
+}
+else{
+    console.log("flase");
+}
---
+++ PrimeNumber.js
@@ -0,0 +1,7 @@
+let num=6;
+for(let i=2; i<num; i++){
+    if(num%i==0){
+    console.log("not prime");
+    break;
+    }
+}
